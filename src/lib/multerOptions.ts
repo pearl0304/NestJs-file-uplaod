a0efb18232f4c402a2dbf23a6ca1db308096d380
Kylie <kylie@comets.kr>
2022-11-03 12:44:58 +0900

    modify randomName

--- src/lib/multerOptions.ts
+++ src/lib/multerOptions.ts
@@ -8,11 +8,9 @@ export const imageFileFilter = (req, file, callback) => {
 export const editFileName = (req, file, callback) => {
   const name = file.originalname.split(".")[0];
   const fileExtName = file.originalname.slice(((file.originalname.lastIndexOf(".") - 1) + 2));
-  const randomName = Array(4)
-    .fill(null)
-    .map(() => Math.round(Math.random() * 16).toString(16))
-    .join("");
-  callback(null, `${name}-${randomName}.${fileExtName}`);
+  const time = Date.now();
+  const randomName = `${name}-${time}`
+  callback(null, `${randomName}.${fileExtName}`);
 };
 
 
